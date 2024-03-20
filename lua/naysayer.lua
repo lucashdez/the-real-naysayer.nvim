0216782bac91fe38c59cf0a3088c239b22265a80
schlanqe <ilhanburcakbas@gmail.com>
2024-03-20 22:11:59 +0300

    :D

--- lua/naysayer.lua
+++ lua/naysayer.lua
@@ -900,6 +900,13 @@ function M.colorscheme(variant)
 	vim.g.colors_name = "naysayer"
 
 	set_highlights()
+    vim.cmd("highlight ColorColumn guibg=NONE")
+    vim.cmd("highlight Comment guifg=#31B72C")
+    vim.cmd("highlight StatusLine guibg=#B59E7A")
+    vim.cmd("highlight StatusLine guifg=#000000")
+    vim.cmd("highlight LineNr guifg=#bdb395")
+    vim.cmd("highlight Nontext guifg=#bdb395")
+    vim.cmd("highlight Visual guifg=#333333 guibg=#82aaa3")
 end
 
 ---@param options Options
